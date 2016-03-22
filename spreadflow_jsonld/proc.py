0a9181bd2cb788f274f9c5051278d592c33131de
Lorenz Schori <lo@znerol.ch>
2016-03-22 07:46:09 +0100

    Add JsonLdUnpack

--- spreadflow_jsonld/proc.py
+++ spreadflow_jsonld/proc.py
@@ -5,7 +5,50 @@ from __future__ import unicode_literals
 import collections
 from pyld import jsonld
 
-from spreadflow_delta.proc import MapReduceBase
+from spreadflow_delta.proc import MapReduceBase, UnpackBase
+
+
+class JsonLdUnpackBase(UnpackBase):
+    """
+    Generates JSON-LD keywords (e.g. @id, @context, @type) for the given
+    subjects and replaces embedded objects with subject references.
+    """
+
+    def _replace_by_reference(self, value, refmap):
+        if id(value) in refmap:
+            return refmap[id(value)]
+        elif isinstance(value, collections.Mapping):
+            return {k: self._replace_by_reference(v, refmap) for k, v in value.items()}
+        elif isinstance(value, collections.Iterable) and not isinstance(value, (str, unicode, bytearray, buffer)):
+            return [self._replace_by_reference(v, refmap) for v in value]
+        else:
+            return value
+
+    def unpack(self, key, doc):
+        subjects = doc[self.key]
+        keywords = [self.keywords(subj) for subj in subjects]
+        pairs = zip(subjects, keywords)
+        refmap = {id(subj): {'@id': kws['@id']} for subj, kws in pairs}
+        for subj, kws in pairs:
+            result = {k: self._replace_by_reference(v, refmap) for k, v in subj.items()}
+            result.update(kws)
+            yield result
+
+    def keywords(self, subject):
+        idkw = subject.get('@id', hash(subject))
+        return {'@id': idkw}
+
+
+class JsonLdUnpack(JsonLdUnpackBase):
+    def __init__(self, key, func, *args, **kwds):
+        super(JsonLdUnpack, self).__init__(key)
+        self._unpack_func = func
+        self._unpack_args = args
+        self._unpack_kwds = kwds
+
+    def keywords(self, subject):
+        return self._unpack_func(subject, *self._unpack_args, **self._unpack_kwds)
+
 
 class JsonLdFlatten(MapReduceBase):
     def __init__(self, context=None, options=None):
